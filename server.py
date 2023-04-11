45e065ec1f94460d1fbc396a61bb1152df86839f
Dustin Meyer <dustinmeyer@MacBook-Pro.local>
2023-04-11 18:04:26 -0400

    Adding error message for password inputs

--- server.py
+++ server.py
@@ -1,39 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
 
 
-posts = [
-   {
-   'author': 'Dustin Meyer',
-   'title': 'Blog post 1',
-   'content': "First post",
-   'date_posted': 'April 20, 2018'
-   },
-      {
-   'author': 'Bob Joe',
-   'title': 'Blog post 2',
-   'content': "Second post",
-   'date_posted': 'April 21, 2018'
-   },
-]
-
 @app.route("/")
 @app.route("/home")
 def home():
-   return render_template('home.html', posts=posts)
-
+   return render_template('home.html')
 
 @app.route("/about")
 def about():
    return render_template('about.html', title="about")
 
-@app.route("/register")
+@app.route("/register", methods=('GET', 'POST'))
 def register():
+   # Textfield names: name, email, password, confirmPassword
+   if request.method == "POST":
+      if request.form['password'] != request.form['confirmPassword']:
+         return render_template('register.html', title="register", error="is-invalid", errorMsg="Password must match", inputName=request.form['name'], inputEmail=request.form['email'])
+      # redirect when inputs are valid
+      return redirect('/', code=302)
+      #print(request.form['name'])
    return render_template('register.html', title="register")
 
 if(__name__ == "__main__"):
-   app.run(debug=True)
+   app.run(port=8000, debug=True)
    #app.run(host="0.0.0.0", port=80)
 
 
